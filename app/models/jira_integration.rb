f7891c1c4f647e6dd036730206d4126e16f5467c
Dominika.Mips <dominika.mips@amberbit.com>
2013-11-21 16:33:33 +0100

    authenticates user but still pending

--- app/models/jira_integration.rb
+++ app/models/jira_integration.rb
@@ -35,10 +35,11 @@ class JIRAIntegration < Integration
     https = Net::HTTP.new(authentication_uri.host, authentication_uri.port)
     https.use_ssl = true
     https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    req = Net::HTTP::Post.new(authentication_uri.path)
+    req = Net::HTTP::Get.new(authentication_uri.path)
+    req["Content-Type"] = "application/json"
     req.basic_auth username, password
-    req.body = '{"scopes": ["user", "public_repo", "repo"]}'
     res = https.request(req)
+    puts res.code
     puts res.body
     token = JSON.parse(res.body)["value"]
     if token.present?
@@ -54,7 +55,7 @@ class JIRAIntegration < Integration
   end
 
   def authentication_uri
-    URI('https://jira.atlassian.com/rest/auth/latest/session')
+    URI('https://kaaarmatest.atlassian.net/rest/auth/latest/session')
   end
 
   def authentication_token_uri
