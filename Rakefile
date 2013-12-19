accf1b3cb3fc034dff77bc34509526238b65ea35
Dominika.Mips <dominika.mips@amberbit.com>
2013-12-19 14:46:45 +0100

    rake fix

--- Rakefile
+++ Rakefile
@@ -1,7 +1,6 @@
 #!/usr/bin/env rake
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
-
 require File.expand_path('../config/application', __FILE__)
 
 KarmaTracker::Application.load_tasks
@@ -11,6 +10,5 @@ RDoc::Task.new :rdoc do |rdoc|
 
  rdoc.rdoc_files.include("README.rdoc", "app/controllers/**/*.rb")
  rdoc.title = "KarmaTracker API Documentation"
- rdoc.options << "--all" 
+ rdoc.options << "--all"
 end
-
--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,10 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
 require 'omniauth'
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 
 Bundler.require(:default, Rails.env)
-# if defined?(Bundler)
-#  # If you precompile assets before deploying to production, use this line
-#  Bundler.require(*Rails.groups(:assets => %w(development test)))
 #  # If you want your assets lazily compiled in production, use this line
 #  # Bundler.require(:default, :assets, Rails.env)
 # end
@@ -44,9 +41,6 @@ module KarmaTracker
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
